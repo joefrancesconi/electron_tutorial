148e7e9df8dae98faa46c7f288ca81bdfbc8c775
Joe Francesconi <francesconijoer@gmail.com>
2023-09-28 19:08:26 -0400

    Fixed a typo

--- renderer.js
+++ renderer.js
@@ -2,7 +2,7 @@ const information = document.getElementById('info')
 information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
 
 const func = async () => {
-    const response = await window.version.ping()
+    const response = await window.versions.ping()
     console.log(response)
 }
 
